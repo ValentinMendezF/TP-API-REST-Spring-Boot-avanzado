5dd1c0fb18f72a031b14220b493671820dbab31b
ValentinMendezF <valenmendez727@gmail.com>
2024-09-25 22:22:45 -0300

    TP terminado

--- src/main/java/com/example/inicial1/services/BaseServiceImpl.java
+++ src/main/java/com/example/inicial1/services/BaseServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.inicial1.services;
 import com.example.inicial1.entities.Base;
 import com.example.inicial1.repositories.BaseRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.io.Serializable;
 import java.util.List;
@@ -19,6 +21,16 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
     public List<E> findAll() throws Exception {
         try {
             List<E> entities = baseRepository.findAll();
+            return entities;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+    @Override
+    @Transactional
+    public Page<E> findAll(Pageable pageable) throws Exception{
+        try {
+            Page<E> entities = baseRepository.findAll(pageable);
             return entities;
         } catch (Exception e) {
             throw new Exception(e.getMessage());
